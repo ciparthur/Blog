8335a49442e92e0196a09e7801bbb4845b5618c6
Arthur <cipitelliarthurgabriel@protonmail.com>
2021-12-27 17:16:04 -0300

    Restrição dos usuários
    Adicionado restrição as funções e páginas do site, para que apenas usuários logados possam adicionar e editar suas próprias publicações.

--- blogs/models.py
+++ blogs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class BlogPost(models.Model):
+    proprietario = models.ForeignKey(User, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=50)
     texto = models.TextField()
     data_add = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.titulo
--- blogs/views.py
+++ blogs/views.py
@@ -1,56 +1,69 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import BlogPost
 from .forms import NovoPost, EditarPost
 
+
+def verificar_proprietario_pub(proprietario, usuario):
+    """Verifica se o usuário é proprietário da conta"""
+    if proprietario != usuario:
+        raise Http404('Página não encontrada :(')
+
+
 def index(request):
     posts = BlogPost.objects.all().order_by('-data_add')
-    
+
     contexto = {'posts': posts}
-    
+
     return render(request, 'blogs/index.html', contexto)
 
+
+@login_required
 def novo_post(request):
-    post = BlogPost.objects.all()
+    """Função view para adicionar novas publicações ao blog"""
 
     if request.method == 'POST':
         formulario = NovoPost(request.POST)
 
         if formulario.is_valid():
-            post = BlogPost.objects.create()
+            post = formulario.save(commit=False)
 
-            post.titulo = formulario.cleaned_data['titulo']
-            post.texto = formulario.cleaned_data['texto']
+            post.proprietario = request.user
 
             post.save()
-            
+
             return HttpResponseRedirect(reverse('index'))
     else:
         formulario = NovoPost()
 
-    contexto = {'formulario': formulario, 'post': post}
+    contexto = {'formulario': formulario}
 
     return render(request, 'blogs/novo_post.html', contexto)
 
+
+@login_required
 def editar_post(request, post_id):
     post = BlogPost.objects.get(id=post_id)
-    
+
+    verificar_proprietario_pub(post.proprietario, request.user)
+
     if request.method == 'POST':
         formulario = EditarPost(data=request.POST, instance=post)
-        
+
         if formulario.is_valid():
             post.titulo = formulario.cleaned_data['titulo']
             post.texto = formulario.cleaned_data['texto']
-            
+
             post.save()
-            
+
             return HttpResponseRedirect(reverse('index'))
 
     else:
         formulario = EditarPost(instance=post)
-    
+
     contexto = {'formulario': formulario, 'post': post}
-    
+
     return render(request, 'blogs/editar_post.html', contexto)
